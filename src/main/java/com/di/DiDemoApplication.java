7f481f21f1b609df9342648eebced63d1d1f7b68
Kulbhushan.Saxena <Kulbhushan.Saxena@thomsonreuters.com>
2020-01-20 20:46:03 +0530

    Di Controller

--- src/main/java/com/di/DiDemoApplication.java
+++ src/main/java/com/di/DiDemoApplication.java
@@ -2,12 +2,17 @@ package com.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.di.controllers.DiController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		DiController di = (DiController)ctx.getBean("diController");
+		di.sayHello();
 	}
 
 }
---
+++ src/main/java/com/di/controllers/DiController.java
@@ -0,0 +1,13 @@
+package com.di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DiController {
+
+	public String sayHello() {
+		System.out.println("Heloooo!!!!!");
+		return null;
+	}
+
+}
