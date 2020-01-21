2242e999297415bbec44813b09c1a89ab4df95bc
Kulbhushan.Saxena <Kulbhushan.Saxena@thomsonreuters.com>
2020-01-21 09:38:26 +0530

    Removing autowire from constructor based injection

--- src/main/java/com/di/controllers/ConstructorInjectionController.java
+++ src/main/java/com/di/controllers/ConstructorInjectionController.java
@@ -1,6 +1,5 @@
 package com.di.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.di.services.GreetingService;
